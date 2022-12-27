0d7c60d52f8ff99953c5aa9eba827e47fbe99f42
mehdisellami <mehdisellami9@gmail.com>
2022-12-27 16:20:17 +0100

    adding to backRepo

---
+++ src/main/java/com/c2c/dilevery/DeliveryPApplication.java
@@ -0,0 +1,13 @@
+package com.c2c.dilevery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryPApplication.class, args);
+	}
+
+}
